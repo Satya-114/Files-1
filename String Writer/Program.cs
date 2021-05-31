db4d1416e42c30ebd9ee31c38a48d4b7a161669b
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-31 21:47:00 +0530

    Add files via upload

---
+++ String Writer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace String_Writer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Hello, Welcome to the javatpoint \n" +
+                "It is nice site. \n" +
+                "It provides technical tutorials";
+
+            StringBuilder sb = new StringBuilder();
+            StringWriter writer = new StringWriter(sb);
+
+            writer.WriteLine(text);
+            writer.Flush();
+
+            writer.Close();
+
+            StringReader reader = new StringReader(sb.ToString());
+
+            while (reader.Peek() > -1)
+            {
+                Console.WriteLine(reader.ReadLine());
+            }
+        }
+    }
+}
+    
+
